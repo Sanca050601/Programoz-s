e69437db581e9528f36afdd3523f38b4cba3f2b6
Sanca050601 <90603494+Sanca050601@users.noreply.github.com>
2021-11-10 14:47:07 +0100

    Add files via upload

---
+++ Dolgozat 11.10.py
@@ -0,0 +1,39 @@
+print('1. feladat')
+print('Himnusz')
+
+print('Isten, áldd meg a magyart')
+print('Jó kedvvel, bőséggel,')
+print('Nyújts feléje védő kart,')
+print('Ha küzd ellenséggel')
+
+print('2. feladat')
+
+n = input('Név: ')
+x = int(input('Születési év: '))
+y = int(input('Születési hónap: '))
+s = int(input('Születési nap: '))
+
+print('3. feladat')
+
+a = 100
+b = 200
+
+print('Csere előtt: ', a,b)
+print('Csere után: ', b,a)
+
+print('4. feladat')
+
+f = int(input('A téglalap egyik oldala: '))
+j = int(input('A téglalap másik oldala: '))
+
+print('A téglalap kerülete: ', 2*(f+j))
+print('A téglalap területe: ', f*j)
+
+print('5. feladat')
+
+i = int(input('A derékszögű háromszög egyik oldala: '))
+o = int(input('A derékszögű háromszög másik oldala: '))
+l = int(input('A derékszögű háromszög harmadik oldala: '))
+
+print('A derékszögű háromszög kerülete: ', i+o+l)
+print('A derékszögű háromszög területe: ', i+o/2)
